ca326d46db739cb1694ad829ffeb8e2af0b04f77
Ori Zaken <zaken1ori@gmail.com>
2021-06-02 21:06:43 +0300

    in progress

---
+++ js/userPrefController.js
@@ -0,0 +1,60 @@
+'use strict';
+
+function onColorPrefChange(event) {
+
+    event.preventDefault();
+    prefChangeNotice();
+    var elNewBgc = document.querySelector('input[name = "bgc"]');
+    var elNewTc = document.querySelector('input[name = "tc"]');
+
+    gPref = { bgc: elNewBgc.value, textColor: elNewTc.value };
+    setUserPrefChanges(gPref);
+    _savePreferencesToStorage();
+}
+
+async function onDatePrefChange(event, hasZodiac = null) {
+
+    if (event) event.preventDefault();
+
+    var elNewDate = document.querySelector('input[name = "dob"]');
+
+    gZodiac = zodiacSignHelper(elNewDate.value);
+    if (gZodiac === undefined) {
+
+        prefChangeNotice(false);
+        return;
+
+    } else prefChangeNotice();
+
+
+    await getAstrologyForecast(gZodiac);
+
+    _savePreferencesToStorage();
+}
+
+function setHomepageIndex() {
+
+    var elForecast = document.querySelector('.dailyForecast');
+}
+
+function prefChangeNotice(success = true) {
+
+    var elNotice = document.querySelector('.settings-message');
+    if (success) {
+        elNotice.textContent = 'Settings Saved Successfully!';
+    } else {
+
+        elNotice.textContent = 'Settings Unchanges - Pick A Date!';
+    }
+    elNotice.style.opacity = '1';
+
+    setTimeout(() => {
+
+        elNotice.style.opacity = '0';
+    }, 2000);
+}
+
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
---
+++ js/userPrefService.js
@@ -0,0 +1,143 @@
+'use strict';
+
+var gPref;
+var gForecast;
+var gZodiac;
+const KEYp = 'prefData';
+var gUserData;
+
+
+function setUserPrefChanges(preferences) {
+    document.body.style.backgroundColor = preferences.bgc;
+    document.body.style.color = preferences.textColor;
+}
+
+function _savePreferencesToStorage() {
+    gUserData = { gPref, gZodiac };
+    saveToStorage(KEYp, gUserData);
+}
+
+function zodiacSignHelper(newDate) {
+
+    var birthDate = newDate.split('-');
+    birthDate = birthDate.splice(1);
+    birthDate = { month: parseInt(birthDate[0]), day: parseInt(birthDate[1]) };
+    console.log(birthDate);
+
+    return determineZodiacByDate(birthDate);
+}
+
+async function getAstrologyForecast(zodiac) {
+
+    const URL = `https://aztro.sameerkumar.website/?sign=${zodiac}&day=today`;
+    await fetch(URL, {
+            method: 'POST'
+        })
+        .then(response => response.json())
+        .then(json => { gForecast = json; });
+}
+
+function determineZodiacByDate(date) {
+
+    switch (date.month) {
+
+        case 1: // january
+            if (date.day >= 20) {
+                return 'aquarius';
+            } else {
+                return 'capricorn';
+            }
+            break;
+
+        case 2: // february
+            if (date.day >= 19) {
+                return 'pisces';
+            } else {
+                return 'aquarius';
+            }
+            break;
+
+        case 3: // march
+            if (date.day >= 21) {
+                return 'aries';
+            } else {
+                return 'pisces';
+            }
+            break;
+
+        case 4: // april
+            if (date.day >= 20) {
+                return 'taurus';
+            } else {
+                return 'aries';
+            }
+            break;
+
+        case 5: // may
+            if (date.day >= 21) {
+                return 'gemini';
+            } else {
+                return 'taurus';
+            }
+            break;
+
+        case 6: // june
+            if (date.day >= 21) {
+                return 'cancer';
+            } else {
+                return 'gemini';
+            }
+            break;
+
+        case 7: // july
+            if (date.day >= 23) {
+                return 'leo';
+            } else {
+                return 'cancer';
+            }
+            break;
+
+        case 8: // august
+            if (date.day >= 23) {
+                return 'virgo';
+            } else {
+                return 'leo';
+            }
+            break;
+
+        case 9: // september
+            if (date.day >= 23) {
+                return 'libra';
+            } else {
+                return 'virgo';
+            }
+            break;
+
+        case 10: // october
+            if (date.day >= 23) {
+                return 'scorpio';
+            } else {
+                return 'libra';
+            }
+            break;
+
+
+        case 11: // november
+            if (date.day >= 22) {
+                return 'sagittarius';
+            } else {
+                return 'scorpio';
+            }
+            break;
+
+
+        case 12: // december
+            if (date.day >= 22) {
+                return 'capricorn';
+            } else {
+                return 'sagittarius';
+            }
+            break;
+
+    }
+}
